0bc7b37653f35ef4d75eaeb0d61526ff6bd2a505
yezi <744177929@qq.com>
2026-05-12 23:15:37 +0800

    重构项目结构，使用DDD/整洁架构模式

---
+++ src/main/java/com/example/testinglab/common/error/GlobalExceptionHandler.java
@@ -0,0 +1,38 @@
+package com.example.testinglab.common.error;
+
+import com.example.testinglab.order.domain.OrderNotFoundException;
+import jakarta.validation.ConstraintViolationException;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(OrderNotFoundException.class)
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    public ErrorResponse handleOrderNotFound(OrderNotFoundException exception) {
+        return new ErrorResponse("ORDER_NOT_FOUND", exception.getMessage());
+    }
+
+    @ExceptionHandler(ConstraintViolationException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public ErrorResponse handleConstraintViolation(ConstraintViolationException exception) {
+        return new ErrorResponse("INVALID_REQUEST", exception.getMessage());
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public ErrorResponse handleMethodArgumentNotValid(MethodArgumentNotValidException exception) {
+        String message = exception.getBindingResult()
+                .getFieldErrors()
+                .stream()
+                .findFirst()
+                .map(error -> error.getField() + ": " + error.getDefaultMessage())
+                .orElse("request body is invalid");
+
+        return new ErrorResponse("INVALID_REQUEST", message);
+    }
+}
